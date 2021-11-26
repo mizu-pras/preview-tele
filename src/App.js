f37b1216d5d3a9cb9a5d00fcf73dcbd3151fa1bd
dimas <1-dimas@hris-system.com>
2021-11-26 15:59:12 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
+import {Helmet} from "react-helmet";
 
 function App() {
   useEffect(() => {
-    window.location.href = "https://www.instagram.com/p/CWuUtDBpj7H/?utm_medium=share_sheet";
+    // window.location.href = "https://www.instagram.com/p/CWuUtDBpj7H/?utm_medium=share_sheet";
   }, []);
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>App</h1>
+    </div>
   );
 }
 
